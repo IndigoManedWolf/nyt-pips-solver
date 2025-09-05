2b65ae05e83a543a51b8acefc8c37e8a3609f4de
IndigoManedWolf <77252684+IndigoManedWolf@users.noreply.github.com>
2025-09-04 23:47:04 -0500

    Update pips_solver.py
    I forgot that the width and height checks when filling out the board depend on the width and height, whoops! Added dependency on those now.

--- pips_solver.py
+++ pips_solver.py
@@ -11,18 +11,20 @@ blank_board = tuple(tuple(-1 for i in range(4)) for j in range(5))
 dominos = [(1,4),(3,4),(5,3),(0,1),(5,1),(0,3),(0,5),(2,5),(4,0),(2,4)]
 checks = {(0,4,5,6,7):lambda x:sum(x)==24, (1,):lambda x:sum(x)>1, (2,):lambda x:sum(x)==0, (8,12,16):lambda x:sum(x)==4, (9,10):lambda x:x[0]==x[1], (13,14,15):lambda x:(x[0] == x[1]) and (x[1] == x[2]), (17,18):lambda x:x[0]==x[1]}
 solutions = []
+board_w = len(blank_board[0])
+board_h = len(blank_board)
 
 def create_arrangement(state):
 	board = [[i for i in j] for j in blank_board]
 	for pos in state:
 		temp = sum(board, start=[]).index(-1)
-		y = temp//4
-		x = temp%4
+		y = temp//board_w
+		x = temp%board_w
 		if pos % 2 == 0:
-			if x+1 == 4 or board[y][x+1] != -1:
+			if x+1 == board_w or board[y][x+1] != -1:
 				return False
 		else:
-			if y+1 == 5:
+			if y+1 == board_h:
 				return False
 		board[y][x] = 1
 		board[y+1 if pos%2 else y][x+1 if pos%2==0 else x] = 1
@@ -37,14 +39,14 @@ def eval_state(state, dominos, checks, verbose=False):
 	board = [[i for i in j] for j in blank_board]
 	for dom, pos in state:
 		temp = sum(board, start=[]).index(-1)
-		y = temp//4
-		x = temp%4
+		y = temp//board_w 
+		x = temp%board_w 
 		if pos % 2 == 0:
-			if x+1 == 4 or board[y][x+1] != -1:
+			if x+1 == board_w or board[y][x+1] != -1:
 				if verbose: print("Failed placing domino", dom, "at", x, ",", y)
 				return False
 		else:
-			if y+1 == 5:
+			if y+1 == board_h:
 				if verbose: print("Failed placing domino", dom, "at", x, ",", y)
 				return False
 		board[y][x] = dominos[dom][1 if pos>= 2 else 0]
@@ -73,13 +75,13 @@ def create_board(state, dominos):
 	board = [[i for i in j] for j in blank_board]
 	for dom, pos in state:
 		temp = sum(board, start=[]).index(-1)
-		y = temp//4
-		x = temp%4
+		y = temp//board_w 
+		x = temp%board_w 
 		if pos % 2 == 0:
-			if x+1 == 4 or board[y][x+1] != -1:
+			if x+1 == board_w or board[y][x+1] != -1:
 				return False
 		else:
-			if y+1 == 5:
+			if y+1 == board_h:
 				return False
 		board[y][x] = dominos[dom][1 if pos>= 2 else 0]
 		board[y+1 if pos%2 else y][x+1 if pos%2==0 else x] = dominos[dom][0 if pos>=2 else 1]
